223c452a240e8827f9f256ed4ec7e726b1bcff4e
harunrashid4419 <harunrashid4419@gmail.com>
2022-12-11 14:35:54 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+const projects = require('./data/projects.json');
+
+app.use(cors());
+app.use(express());
+
+
+app.get('/projects', async(req, res) =>{
+    res.send(projects);
+});
+
+app.get('/projects/:id', async(req, res) =>{
+    const id = req.params.id;
+    const project = projects.find(project => project.id === id);
+    res.send(project);
+})
+
+app.get('/', async(req, res) =>{
+    res.send('port is running');
+})
+
+app.listen(port, () => console.log(`port is running on ${port}`));
